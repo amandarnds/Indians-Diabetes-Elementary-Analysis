68155dfd7ccad9a8f6e84bb1f03b6a30ec50aeeb
Amanda Rodrigues <120829907+amandarnds@users.noreply.github.com>
2023-05-17 21:01:54 -0700

    ✨ feat: add code
    add new analysis code

---
+++ Indian-Diabetes-Elementary-Analysis.R
@@ -0,0 +1,25 @@
+ #1 - Set directory
+
+setwd('F:/github/r/Indians-Diabetes-Elementary-Analysis')
+getwd()
+
+#2 - Upload the dataset
+install.packages("mlbench")
+library(mlbench)
+data(PimaIndiansDiabetes)
+View(PimaIndiansDiabetes)
+
+#3-update the name 
+ds <- PimaIndiansDiabetes
+View(ds)
+
+#4 - Dimension and type of data
+dim(ds)
+str(ds)
+
+#5 - Summary 
+summary(ds)
+
+#6 -Median
+m1 <- mean(ds$age)
+print(m1)
